8491cbf2572c1ed628c524a2d8356d25b18cded4
Matthew Aitken <matt@itken.net>
2024-09-18 10:25:37 +0000

    Add test for date extension

--- student.py
+++ student.py
@@ -25,4 +25,8 @@ class Student:
         self.naughty_list = True
 
     
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    
     
--- test_student.py
+++ test_student.py
@@ -3,24 +3,40 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
 
-        self.assertEqual(student.full_name, 'John Doe')
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setup')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('teardown')
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
 
     def test_email(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.email, 'john.doe@email.com')
+        print('test_email')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
     
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('test_alert_santa')
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
-        self.assertTrue(student.naughty_list)
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertGreater(self.student.end_date, old_end_date)
     
     
-
 
 if __name__ == '__main__':
     unittest.main()
